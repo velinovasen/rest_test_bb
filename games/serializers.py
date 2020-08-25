9d63feb3a72b7b28d4490b60023a71a65e4a7e70
velinovasen <velinovasen@users.noreply.github.com>
2020-08-25 08:46:59 +0300

    'cu'

--- games/serializers.py
+++ games/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from rest_framework.serializers import ModelSerializer
 from .models import Game
 
@@ -7,3 +8,24 @@ class GamesSerializer(ModelSerializer):
         model = Game
         fields = ["team_home", "team_away", "competition", "home_odd",
                   "draw_odd", "away_odd", "prediction", "result", "won", "profit"]
+
+
+class CreateUser(ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "username", "password", "email", "first_name", "last_name"]
+        write_only_fields = ["password",]
+        read_only_fields = ["id",]
+
+        def create(self, validated_data):
+            user = User.objects.create(
+                username=validated_data["username"],
+                email=validated_data["email"],
+                first_name=validated_data["first_name"],
+                last_name=validated_data["last_name"]
+            )
+
+            user.set_password(validated_data["password"])
+            user.save()
+
+            return user
